62876432b69e8abb1a548753b14eda28d963243b
Alex Li <alex.li@pactiv.com>
2019-11-12 22:18:08 +1300

    Change the runtime version checking logic
    now it will read <targetDomain>/versions which should return a multiple-lined application version list
    then compare it with the expected versions read from classpath
    so contextPath defined in happy-versins file are ignored now

--- src/test/java/net/stickycode/plugin/happy/HappyVersionValidatorMojoComponentTest.java
+++ src/test/java/net/stickycode/plugin/happy/HappyVersionValidatorMojoComponentTest.java
@@ -1,48 +1,14 @@
 package net.stickycode.plugin.happy;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.StrictAssertions.assertThat;
-
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
 
 import org.apache.maven.plugin.MojoFailureException;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HappyVersionValidatorMojoComponentTest {
-
-  @Test(timeout = 1000)
-  public void requestFailure() throws MalformedURLException, InterruptedException {
-    HappyVersionValidatorMojo mojo = new HappyVersionValidatorMojo() {
-
-      String getTargetDomain() {
-        return "http://localhost:7878";
-      }
-    };
-    mojo.setupHttpClient();
-    ApplicationValidationCallback queueRequest = mojo.createRequest(new Application("/:blah-1.2"));
-    while (queueRequest.running())
-      Thread.sleep(100);
-
-    assertThat(queueRequest.success()).isFalse();
-  }
-
-  @Test(timeout = 10000)
-  public void requestClose() throws InterruptedException, IOException {
-    try (Blackhole blackhole = new Blackhole();) {
-      ApplicationValidationCallback queueRequest = request(blackhole);
-      assertThat(queueRequest.success()).isFalse();
-    }
-  }
-
-  @Test(timeout = 10000)
-  public void requestTimeout() throws InterruptedException, IOException {
-    try (Blackhole blackhole = new Blackhole(new DroppingBlackholeHandler());) {
-      ApplicationValidationCallback queueRequest = request(blackhole);
-      assertThat(queueRequest.success()).isFalse();
-    }
-  }
 
   private ApplicationValidationCallback request(Blackhole blackhole) throws MalformedURLException, InterruptedException {
     HappyVersionValidatorMojo mojo = new HappyVersionValidatorMojo() {
