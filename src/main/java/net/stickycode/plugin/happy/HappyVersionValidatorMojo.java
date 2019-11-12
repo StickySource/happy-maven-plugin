c5635911363d2d87f20758dfe19f95c2b1527096
Alex Li <alex.li@pactiv.com>
2019-11-12 22:18:08 +1300

    Change the runtime version checking logic
    now it will read <targetDomain>/versions which should return a multiple-lined application version list
    then compare it with the expected versions read from classpath
    so contextPath defined in happy-versins file are ignored now

--- src/main/java/net/stickycode/plugin/happy/HappyVersionValidatorMojo.java
+++ src/main/java/net/stickycode/plugin/happy/HappyVersionValidatorMojo.java
@@ -2,6 +2,7 @@ package net.stickycode.plugin.happy;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,12 +12,24 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
@@ -33,7 +46,7 @@ import static java.lang.String.join;
 
 @Mojo(name = "validate", threadSafe = true, requiresProject = true, defaultPhase = LifecyclePhase.INTEGRATION_TEST, requiresDependencyResolution = ResolutionScope.TEST)
 public class HappyVersionValidatorMojo
-    extends AbstractMojo {
+  extends AbstractMojo {
 
   @Parameter(defaultValue = "${project}", required = true, readonly = true)
   private MavenProject project;
@@ -74,39 +87,64 @@ public class HappyVersionValidatorMojo
 
   @Override
   public void execute()
-      throws MojoExecutionException, MojoFailureException {
+    throws MojoExecutionException, MojoFailureException {
     buildClasspath();
     setupHttpClient();
 
     Instant finish = Instant.now().plusSeconds(retryDurationSeconds);
 
-    ApplicationValidationResults results = new ApplicationValidationResults();
+    Set<String> expectedVersion = new HashSet<>();
+
     for (Application application : loadApplications(versionFiles)) {
-      results.add(createRequest(application));
+      expectedVersion.add(application.getVersion());
     }
 
     getLog().info(String.format("Validating for up to %ds, retrying every %ds as needed",
       retryDurationSeconds, retryPeriodSeconds));
 
-    while (Instant.now().isBefore(finish) && !results.success())
+    boolean allApplicationVersionMatch = false;
+    while (Instant.now().isBefore(finish) && !allApplicationVersionMatch)
       try {
-        getLog().info(String.format("waiting on %d applications ", results.runningCount()));
         Thread.sleep(retryPeriodSeconds * 1000);
-        if (Instant.now().isBefore(finish))
-          for (ApplicationValidationCallback callback : results.failures()) {
-            callback.reset();
-            queueRequest(callback);
+        if (Instant.now().isBefore(finish)) {
+          Set<String> runtimeVersions = getRuntimeAppVersions();
+          List<String> unmatchedVersions = expectedVersion.stream()
+            .filter(version -> !runtimeVersions.contains(version))
+            .collect(Collectors.toList());
+
+          if (unmatchedVersions.isEmpty()) {
+            getLog().info("All expected versions matched");
+            allApplicationVersionMatch = true;
           }
+          else {
+            getLog().info(String.format("Unmatched versions: [ %s ]", String.join(",", unmatchedVersions)));
+          }
+        }
       }
       catch (InterruptedException e) {
         throw new RuntimeException(e);
       }
+  }
 
-    getLog().info(results.toString());
-
-    if (failBuild)
-      if (results.hasFailures())
-        throw new MojoFailureException(results.failureMessage());
+  private Set<String> getRuntimeAppVersions() throws MojoFailureException {
+    Request request = new Request.Builder()
+      .url(getTargetDomain() + "/versions")
+      .build();
+    try {
+      getLog().info(String.format("Calling %s", request.url()));
+      Response response = client.newCall(request).execute();
+      if (response.body() == null || response.body().string().isEmpty()) {
+        throw new MojoFailureException(String.format("Null or empty response from %s", request.url()));
+      }
+      String content = response.body().string();
+      getLog().info("version info read from " + targetDomain + ":");
+      getLog().info(content);
+      String[] lines = content.split("\n");
+      return new HashSet<>(Arrays.asList(lines));
+    }
+    catch (IOException e) {
+      throw new MojoFailureException(String.format("Error calling %s ", request.url()), e);
+    }
   }
 
   void buildClasspath() throws MojoFailureException {
@@ -136,36 +174,63 @@ public class HappyVersionValidatorMojo
   void setupHttpClient() {
     getLog().info(String.format("http client timeouts connect:%dms read:%dms write:%dms",
       connectTimeoutMillis, readTimeoutMillis, writeTimeoutMillis));
-    this.client = new OkHttpClient.Builder()
+    OkHttpClient.Builder builder = new OkHttpClient.Builder()
       .connectTimeout(connectTimeoutMillis, TimeUnit.MILLISECONDS)
       .writeTimeout(writeTimeoutMillis, TimeUnit.MILLISECONDS)
-      .readTimeout(readTimeoutMillis, TimeUnit.MILLISECONDS)
+      .readTimeout(readTimeoutMillis, TimeUnit.MILLISECONDS);
+
+    ignoreSSL(builder);
+
+    this.client = builder
       .build();
   }
 
-  void queueRequest(ApplicationValidationCallback callback) {
-    Request request = new Request.Builder()
-      .url(applicationUrl(callback.getContextPath()))
-      .build();
+  void ignoreSSL(OkHttpClient.Builder builder) {
+    // Create a trust manager that does not validate certificate chains
+    final TrustManager[] trustAllCerts = new TrustManager[] {
+      new X509TrustManager() {
 
-    client.newCall(request).enqueue(callback);
+        @Override
+        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+          throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+          throws CertificateException {
+        }
+
+        @Override
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+          return new java.security.cert.X509Certificate[] {};
+        }
+      }
+    };
+
+    // Install the all-trusting trust manager
+    try {
+      final SSLContext sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+      // Create an ssl socket factory with our all-trusting manager
+      final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+      builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+      builder.hostnameVerifier(new HostnameVerifier() {
+
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+          return true;
+        }
+      });
+    }
+    catch (KeyManagementException | NoSuchAlgorithmException e) {
+      getLog().error("Failed to apply SSL ignorance. ", e);
+    }
   }
 
   ApplicationValidationCallback createRequest(Application application) {
     ApplicationValidationCallback callback = new ApplicationValidationCallback(application);
-    queueRequest(callback);
     return callback;
-  }
-
-  URL applicationUrl(String contextPath) {
-    try {
-      URL url = new URL(getTargetDomain() + contextPath + "version");
-      getLog().info(String.format("add url %s", url.toString()));
-      return url;
-    }
-    catch (MalformedURLException e) {
-      throw new RuntimeException(e);
-    }
   }
 
   String getTargetDomain() {
